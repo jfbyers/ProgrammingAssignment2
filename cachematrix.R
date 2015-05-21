96ec40be7efdc1b14bee97713c6042deb9a601d5
jfbyers <jfbyers@users.noreply.github.com>
2015-05-21 08:51:24 +0200

    Update cachematrix.R
    ellipsis parameter restored

--- cachematrix.R
+++ cachematrix.R
@@ -21,7 +21,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ##  This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 ## If the inverse has already been calculated then the cachesolve should retrieve the inverse from the cache.
-cacheSolve <- function(x) {
+cacheSolve <- function(x,...) {
   ## Return a matrix that is the inverse of the one contained in 'x'
   i <- x[[2,2]]() # the getinverse() function
   if(!is.null(i)) {
@@ -29,7 +29,7 @@ cacheSolve <- function(x) {
     return(i)
   }
   data <- x[[2,1]]()  # the get function
-  i <- solve(data)  # computes the inverse
+  i <- solve(data,...)  # computes the inverse
   x[[1,1]](i)   # sets the inverse in the object
   i   #returns the inverse
 }
